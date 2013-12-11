336a6ba48ceb115b0c41c46686be9bcae1ca169a
Martin Brok Winther <martinbrokwinther@gmail.com>
2013-12-11 17:35:36 +0100

    clean up

--- tasks/scriptincluder.js
+++ tasks/scriptincluder.js
@@ -8,87 +8,73 @@
 
 module.exports = function(grunt) {
 
-  	var _ = require("underscore");
+	var _ = require("underscore");
+	
+	grunt.registerMultiTask('scriptincluder', '"inserts script refrences in to your files"', function() {
+		var fileContent	= "";
+		var filePath = this.data.dest;
+		var options = this.options({rootPath: ""});
+		var scriptIncludeBuilder = new ScriptTagBuilder(options.rootPath);
+		var match_includeScript = /<!--(?:\s)?\[(?:\s)?@include_scripts(?:\s)?\](?:\s)?-->/gi;
 
+		if(fileExist(filePath)){
+			fileContent = grunt.file.read(filePath);
+			if(hasIncludeScriptTag(fileContent)){
+				var files = extractFilePaths(this.data.src);
+				var scriptIncludes = scriptIncludeBuilder.create(files);
+				var newFileContent = includeScripts(fileContent, scriptIncludes);
+				grunt.file.write(filePath, newFileContent, "utf8");
+			}
+		}
 
-  	
-  	grunt.registerMultiTask('scriptincluder', '"inserts script refrences in to your files"', function() {
-
-
-  		var options = this.options({rootPath: ""});
-	   	var filePath = this.data.dest;
-		var fileContent = "";
-		var scriptBuilder = new ScriptTagBuilder(options.rootPath);
-		
-		// Matches: <!-- [@include_scripts] -->
-		var regex_includeTag = /<!--(?:\s)?\[(?:\s)?@include_scripts(?:\s)?\](?:\s)?-->/gi;
-
-
-  		function extractFilePaths(data) {
-	    	var files = _.flatten(data);
-	    	var expandedFileGlobs = [];
-	    	files.forEach(function (file) {
+		function extractFilePaths(data) {
+			var files = _.flatten(data);
+			var expandedFileGlobs = [];
+			files.forEach(function (file) {
 				var tempFiles = grunt.file.expand(file);
 				if(tempFiles.length)
 					expandedFileGlobs = expandedFileGlobs.concat(tempFiles);
-	      	})
-	      	return expandedFileGlobs;
-	    }
+			})
+			return expandedFileGlobs;
+		}
 
-	    function fileExist(file) {
+		function fileExist(file) {
 			return grunt.file.exists(file);    	
-	    }
+		}
 
-	    function hasIncludeScriptTag (source) {
-	    	return regex_includeTag.test(source);
-	    }
+		function hasIncludeScriptTag (source) {
+			return match_includeScript.test(source);
+		}
 
-	    function includeScripts (source, scriptIncludes) {
-	    	return source.replace(regex_includeTag, scriptIncludes).toString();
-	    }
-
-
-			if(fileExist(filePath)){
-				fileContent = grunt.file.read(filePath);
-				if(hasIncludeScriptTag(fileContent)){
-					var files = extractFilePaths(this.data.src);
-					var scriptIncludes = scriptBuilder.create(files);
-					var newFileContent = includeScripts(fileContent, scriptIncludes);
-					grunt.file.write(filePath, newFileContent, "utf8");
-				}
-			}
-		
-
-		
-
-	
-
-  	})
+		function includeScripts (source, scriptIncludes) {
+			return source.replace(match_includeScript, scriptIncludes).toString();
+		}
+  })
 
   	/** @internal class */
 	function ScriptTagBuilder (rootPath) {
 
-		/** @public */
 		this.create = function (files) {
-			return 	createScriptIncludeHeader() 	+ 
-		   			createScriptIncludeBody(files) 	+
-		   			createScriptIncludeFotter();
+		  return  createScriptIncludeHeader() 	+ 
+              createScriptIncludeBody(files) 	+
+		   			  createScriptIncludeFotter();
 		}
-		/* private 
-		**************************************************/
+
 		var createScriptIncludeHeader = function() {
 			return "\t<!-- Generated Script includes -->\n";
 		}
-		var createScriptIncludeBody = function(files) {
-	    	var scriptTags = "";
-	    	console.log("creating includes")
-	    	files.forEach(function(file){
 
-	    		console.log("\t", rootPath + file)
-	    		scriptTags += "\t<script src='" + rootPath + file + "'></script>\n";
-	    	});
-	    	return scriptTags;
+		var createScriptIncludeBody = function(files) {
+    	var scriptTags = "";
+    	console.log("creating includes")
+    	files.forEach(function(file){
+
+    		console.log("\t", rootPath + file)
+    		scriptTags += "\t<script src='" + rootPath + file + "'></script>\n";
+    	});
+    	return scriptTags;
 		}
+
 		var createScriptIncludeFotter = function() {
 			return "\t<!-- /Generated Script includes -->\n";
 		}
--- test/scriptinicluder_test.js
+++ test/scriptinicluder_test.js
@@ -23,49 +23,42 @@ var grunt = require('grunt');
 */
 
 
-
+function readFileAndNormalizeContent (filePath) {
+  return grunt.file.read(filePath).replace(/[\s\t\r\n]+/gi, "");
+}
 
 exports.scriptinicluder = {
+  
   setUp: function(done) {
-    // setup here if necessary
     done();
   },
 
   it_should_include_files_in_to_htmlpage: function(test) {
+    var actual   = readFileAndNormalizeContent("./tmp/page1.html");
+    var expected = readFileAndNormalizeContent("./test/expected/page1.html");
+    
     test.expect(1);
-
-
-    var expected = grunt.file.read("./test/expected/page1.html").replace(/[\s\t\r\n]+/gi, "");
-    var actual   = grunt.file.read("./tmp/page1.html").replace(/[\s\t\r\n]+/gi, "");
-
     test.equal(expected, actual, "")
-
-    
-    
-    
-
     test.done();
   },
 
   it_should_include_files_for_multiple_targets: function (test) {
+    var actual1   = readFileAndNormalizeContent("./tmp/page1.html");
+    var actual2   = readFileAndNormalizeContent("./tmp/page2.html");
+    var expected1 = readFileAndNormalizeContent("./test/expected/page1.html");
+    var expected2 = readFileAndNormalizeContent("./test/expected/page2.html");
+
     test.expect(2);
-
-    var expected1 = grunt.file.read("./test/expected/page1.html").replace(/[\s\t\r\n]+/gi, "");
-    var actual1   = grunt.file.read("./tmp/page1.html").replace(/[\s\t\r\n]+/gi, "");
-
-    var expected2 = grunt.file.read("./test/expected/page2.html").replace(/[\s\t\r\n]+/gi, "");
-    var actual2   = grunt.file.read("./tmp/page2.html").replace(/[\s\t\r\n]+/gi, "");
-
     test.equal(expected1, actual1, "")
     test.equal(expected2, actual2, "")
-
     test.done();
   },
 
   it_should_correctly_include_files_on_config_reference: function (test) {
+    var actual   = readFileAndNormalizeContent("./tmp/page3.html");
+    var expected = readFileAndNormalizeContent("./test/expected/page3.html");
+    
     test.expect(1);
-    var expected = grunt.file.read("./test/expected/page3.html").replace(/[\s\t\r\n]+/gi, "");
-    var actual   = grunt.file.read("./tmp/page3.html").replace(/[\s\t\r\n]+/gi, "");
     test.equal(expected, actual, "");
     test.done();
   }
