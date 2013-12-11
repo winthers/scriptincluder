65d9045af80fecc71f64c50addb4ce3c8c14fe03
Martin Brok Winther <martinbrokwinther@gmail.com>
2013-12-11 17:48:18 +0100

    clean up

--- tasks/scriptincluder.js
+++ tasks/scriptincluder.js
@@ -8,56 +8,56 @@
 
 module.exports = function(grunt) {
 
-	var _ = require("underscore");
-	
+  var _ = require("underscore");
+
   grunt.registerMultiTask('scriptincluder', '"inserts script refrences in to your files"', function() {
-		var fileContent	= "";
-		var filePath = this.data.dest;
-		var options = this.options({rootPath: ""});
-		var scriptIncludeBuilder = new ScriptTagBuilder(options.rootPath);
-		var match_includeScript = /<!--(?:\s)?\[(?:\s)?@include_scripts(?:\s)?\](?:\s)?-->/gi;
+    var fileContent = "";
+    var filePath = this.data.dest;
+    var options = this.options({rootPath: ""});
+    var scriptIncludeBuilder = new ScriptTagBuilder(options.rootPath);
+    var match_includeScript = /<!--(?:\s)?\[(?:\s)?@include_scripts(?:\s)?\](?:\s)?-->/gi;
 
-		if(fileExist(filePath)){
-			fileContent = grunt.file.read(filePath);
-			if(hasIncludeScriptTag(fileContent)){
-				var files = extractFilePaths(this.data.src);
-				var scriptIncludes = scriptIncludeBuilder.create(files);
-				var newFileContent = includeScripts(fileContent, scriptIncludes);
-				grunt.file.write(filePath, newFileContent, "utf8");
-			}
-		}
+    if (fileExist(filePath)) {
+      fileContent = grunt.file.read(filePath);
+      if (hasIncludeScriptTag(fileContent)) {
+        var files = extractFilePaths(this.data.src);
+        var scriptIncludes = scriptIncludeBuilder.create(files);
+        var newFileContent = includeScripts(fileContent, scriptIncludes);
+        grunt.file.write(filePath, newFileContent, "utf8");
+      }
+    }
 
-		function extractFilePaths(data) {
-			var files = _.flatten(data);
-			var expandedFileGlobs = [];
-			files.forEach(function (file) {
-				var tempFiles = grunt.file.expand(file);
-				if(tempFiles.length)
-					expandedFileGlobs = expandedFileGlobs.concat(tempFiles);
-			})
-			return expandedFileGlobs;
-		}
+    function extractFilePaths(data) {
+      var files = _.flatten(data);
+      var expandedFileGlobs = [];
+      files.forEach(function(file) {
+        var tempFiles = grunt.file.expand(file);
+        if (tempFiles.length)
+          expandedFileGlobs = expandedFileGlobs.concat(tempFiles);
+      })
+      return expandedFileGlobs;
+    }
 
-		function fileExist(file) {
-			return grunt.file.exists(file);    	
-		}
+    function fileExist(file) {
+      return grunt.file.exists(file);
+    }
 
-		function hasIncludeScriptTag (source) {
-			return match_includeScript.test(source);
-		}
+    function hasIncludeScriptTag(source) {
+      return match_includeScript.test(source);
+    }
 
-		function includeScripts (source, scriptIncludes) {
-			return source.replace(match_includeScript, scriptIncludes).toString();
-		}
+    function includeScripts(source, scriptIncludes) {
+      return source.replace(match_includeScript, scriptIncludes).toString();
+    }
   })
 
   /** @internal class */
-  function ScriptTagBuilder (rootPath) {
+  function ScriptTagBuilder(rootPath) {
 
-    this.create = function (files) {
-      return  createScriptIncludeHeader() 	+ 
-              createScriptIncludeBody(files) 	+
-              createScriptIncludeFotter();
+    this.create = function(files) {
+      return createScriptIncludeHeader() +
+        createScriptIncludeBody(files) +
+        createScriptIncludeFotter();
     }
 
     var createScriptIncludeHeader = function() {
@@ -68,11 +68,11 @@ module.exports = function(grunt) {
       var scriptTags = "";
 
       console.log("creating includes")
-      files.forEach(function(file){
+      files.forEach(function(file) {
         console.log("\t", rootPath + file)
         scriptTags += "\t<script src='" + rootPath + file + "'></script>\n";
       });
-      
+
       return scriptTags;
     }
 
